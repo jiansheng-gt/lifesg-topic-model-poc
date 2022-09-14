ac922b9270f4148c5e6cc4e64e172359396e7c28
Jian Sheng <ang_jian_sheng@tech.gov.sg>
2022-09-14 15:37:58 +0800

    [MOL-11173][JS|AMI] add spacy tokenizer and lemmatizer

--- topic_modeling/common/preprocessing.py
+++ topic_modeling/common/preprocessing.py
@@ -1,20 +1,38 @@
-import re
 import nltk
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4') # for wordnet lemmatizer
+
+import spacy
 
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
 
+# spacy nlp
+parser = spacy.load('en_core_web_sm')
+lemmatizer = parser.get_pipe("lemmatizer")
+
 # stopwords, might want to extend
 en_stop = stopwords.words('english')
-# en_stop = set('for a of the and to in'.split())
 
 def tokenize(text):
-    return text.lower().split()
+    tokens = []
+    doc = parser(text)
+
+    for token in doc:
+        if token.is_space:
+            continue
+        if token.like_url:
+            continue
+        if token.is_alpha:
+            # use spacy lemmatizer as well
+            tokens.append(token.lemma_.lower())
+
+            # uncomment this if using other lemmatizer
+            # tokens.append(token.lower_)
+    return tokens
 
 def get_lemma(word):
     return WordNetLemmatizer().lemmatize(word)
@@ -28,8 +46,11 @@ def preprocess(documents):
     # tokenize
     texts = [tokenize(document) for document in documents]
 
-    # remove special characters
-    texts = [re.sub('[^A-Za-z0-9.]+', ' ', document) for document in documents]
+    # remove single characters
+    texts = [
+        [token for token in text if len(token) > 1]
+        for text in texts
+    ]
 
     # bigrams?
 
@@ -40,10 +61,11 @@ def preprocess(documents):
     # ]
 
     # lemmatize - same meaning words
-    texts = [
-        [get_lemma(token) for token in text]
-        for text in texts
-    ]
+    # comment out if using spacy lemmatizer
+    # texts = [
+    #     [get_lemma(token) for token in text]
+    #     for text in texts
+    # ]
 
     # filter stop words
     texts = [
