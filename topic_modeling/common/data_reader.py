da9ac9b303f8d15950dbe626bc62d8496a5d3a95
Jian Sheng <ang_jian_sheng@tech.gov.sg>
2022-09-14 15:36:28 +0800

    [MOL-11173][JS|AMI] sort document parsing and add get_filenames method

--- topic_modeling/common/data_reader.py
+++ topic_modeling/common/data_reader.py
@@ -3,7 +3,11 @@ import os
 class DataReader(object):
     def __init__(self, dirname):
         self.dirname = dirname
+        self.filenames = sorted(os.listdir(self.dirname))
 
     def __iter__(self):
-        for fname in os.listdir(self.dirname):
+        for fname in self.filenames:
             yield open(os.path.join(self.dirname, fname)).read()
+
+    def get_filenames(self):
+        return self.filenames
