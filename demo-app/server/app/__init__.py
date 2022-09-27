6f458e4a6a0c4062697ab404b266fac5dd7c7bc9
Jian Sheng <ang_jian_sheng@tech.gov.sg>
2022-09-27 17:31:45 +0800

    [MOL-11173][JS|AMI] recommendation to frontend
    - update sentence transformer code to output csv to be consumed by flask server
    - add flask server which pulls embeddings from csv and serves recommendations from /guide-recs

---
+++ demo-app/server/app/__init__.py
@@ -0,0 +1,48 @@
+import os
+from flask import request, Flask
+from flask_cors import CORS
+import pandas as pd
+from sentence_transformers import util
+import numpy as np
+
+CSV_PATH = '../data/embeddings.csv'
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+
+    embeddings = pd.read_csv(os.path.join(app.root_path, CSV_PATH))
+    embeddings.embedding = embeddings.embedding.apply(lambda x: np.array(x.split()).astype(np.float64))
+
+    # a simple page that says hello
+    @app.route('/guide-recs', methods=['POST'])
+    def guide_recs():
+        # get user clicks (req body)
+        # TODO: handle empty body
+        data = request.get_json(silent=True)
+
+        # 1 click = 1 embedding
+        user_embeddings = []
+        for guide_id, clicks in data.items():
+            for i in range(clicks):
+                user_embeddings.append(embeddings.loc[int(guide_id)].embedding)
+
+        user_embeddings_mean = np.array(user_embeddings).mean(0)
+
+        user_recs = []
+        # calculate cos sim
+        for index, row in embeddings.iterrows():
+            sim = util.cos_sim([user_embeddings_mean], [row.embedding])
+
+            user_recs.append({
+                'id': index,
+                'title': row.guide_name,
+                'url': row.url,
+                'sim': sim[0][0].item()
+            })
+
+        sorted_recs = sorted(user_recs, key = lambda x: x['sim'], reverse=True)
+        return sorted_recs
+
+    return app
--- topic_modeling/sentence_transformers/start.py
+++ topic_modeling/sentence_transformers/start.py
@@ -6,6 +6,8 @@ sys.path.append(os.getcwd())
 
 from topic_modeling.common.data_reader import DataReader
 from sentence_transformers import SentenceTransformer, util
+import pandas as pd
+import numpy as np
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
@@ -17,40 +19,41 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # all-mpnet-base-v2 - 2 min 22 seconds
 model = SentenceTransformer('all-mpnet-base-v2')
 
-GUIDE_URLS = [
-    "/guides/having-a-baby",
-    "/guides/enrolling-in-preschool",
-    "/government-services/buy-hdb/",
-    "/guides/retrenchment-benefits-and-measures",
-    "/guides/support-for-your-job-search",
-    "/government-services/stay-healthy/",
-    "/government-services/finances/health-expenses/",
-    "/government-services/finances/family/",
-    "/guides/funding-your-retirement",
-    "/guides/active-ageing",
-    "/guides/healthcare-financial-assistance",
-    "/guides/financial-assistance",
-    "/guides/life-after-heart-attack",
-    "/guides/education-career-opportunities",
-    "/guides/senior-care-services",
-    "/guides/resolving-employment-disputes",
-    "/guides/becoming-a-caregiver",
-    "/guides/caregiver-stress",
-    "/guides/p1-registration",
-    "/guides/domestic-helper",
-    "/guides/confinement-nanny",
-    "/guides/bringing-newborn-home",
-];
+GUIDE_DATA = [
+    ["Having A Baby", "https://www.life.gov.sg/guides/having-a-baby"],
+    ["Enrolling In Preschool", "https://www.life.gov.sg/guides/enrolling-in-preschool"],
+    ["Buy Hdb", "https://services.life.gov.sg/government-services/buy-hdb/"],
+    ["Retrenchment Benefits And Measures", "https://www.life.gov.sg/guides/retrenchment-benefits-and-measures"],
+    ["Support For Your Job Search", "https://www.life.gov.sg/guides/support-for-your-job-search"],
+    ["Stay Healthy", "https://services.life.gov.sg/government-services/stay-healthy/"],
+    ["Finances - Health Expenses", "https://services.life.gov.sg/government-services/finances/health-expenses/"],
+    ["Finances - Family", "https://services.life.gov.sg/government-services/finances/family/"],
+    ["Funding Your Retirement", "https://www.life.gov.sg/guides/funding-your-retirement"],
+    ["Active Ageing", "https://www.life.gov.sg/guides/active-ageing"],
+    ["Healthcare Financial Assistance", "https://www.life.gov.sg/guides/healthcare-financial-assistance"],
+    ["Financial Assistance", "https://www.life.gov.sg/guides/financial-assistance"],
+    ["Life After Heart Attack", "https://www.life.gov.sg/guides/life-after-heart-attack"],
+    ["Education Career Opportunities", "https://www.life.gov.sg/guides/education-career-opportunities"],
+    ["Senior Care Services", "https://www.life.gov.sg/guides/senior-care-services"],
+    ["Resolving Employment Disputes", "https://www.life.gov.sg/guides/resolving-employment-disputes"],
+    ["Becoming A Caregiver", "https://www.life.gov.sg/guides/becoming-a-caregiver"],
+    ["Caregiver Stress", "https://www.life.gov.sg/guides/caregiver-stress"],
+    ["P1 Registration", "https://www.life.gov.sg/guides/p1-registration"],
+    ["Domestic Helper", "https://www.life.gov.sg/guides/domestic-helper"],
+    ["Confinement Nanny", "https://www.life.gov.sg/guides/confinement-nanny"],
+    ["Bringing Newborn Home", "https://www.life.gov.sg/guides/bringing-newborn-home"],
+]
 OUTPUT_DIR = 'output'
 CHUNK_SIZE = 500
 model.max_seq_length = 512
 
-def get_guide_name(document_name):
+def get_guide_data(document_name):
     guide_num = int(document_name.split()[1].split('.')[0])
-    return GUIDE_URLS[guide_num - 1]
+    data = GUIDE_DATA[guide_num - 1]
+    return data[0], data[1]
 
 logging.info('Reading data...')
-doc_reader = DataReader('data')
+doc_reader = DataReader('data-with-external')
 documents = list(doc_reader)
 filenames = doc_reader.filenames
 
@@ -70,18 +73,33 @@ for text_chunked in texts_chunked:
     encoding_mean = chunk_sims.mean(0)
     embeddings.append(encoding_mean)
 
+logging.info('Saving embeddings to CSV...')
+header = ['guide_name', 'url', 'filename', 'embedding']
+df = pd.DataFrame(columns=header)
+for index, filename in enumerate(filenames):
+    guide_name, url = get_guide_data(filename)
+    embedding_arr = np.array(embeddings[index])
+    df.loc[index] = [
+        guide_name,
+        url,
+        filename,
+        ' '.join([str(item) for item in embedding_arr])
+    ]
+
+df.to_csv(os.path.join(OUTPUT_DIR, 'embeddings.csv'))
+
 logging.info('Caclculating cosine similarity...')
 doc_sims = {}
 similarities = util.cos_sim(embeddings, embeddings)
 for index, name in enumerate(filenames):
     similarity = []
-    curr_guide_name = get_guide_name(name)
+    curr_guide_name, *_ = get_guide_data(name)
     logging.info('calculating similarity for guide: ' + curr_guide_name)
 
     for comp_index, comp_name in enumerate(filenames):
         if index == comp_index:
                 continue
-        comp_guide_name = get_guide_name(comp_name)
+        comp_guide_name, *_ = get_guide_data(comp_name)
 
         sim = similarities[index][comp_index]
 
