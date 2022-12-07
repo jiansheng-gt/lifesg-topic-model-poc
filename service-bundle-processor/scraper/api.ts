9ca312b287b21c6d543802bad4c39337a43cf1fe
Jian Sheng <ang_jian_sheng@tech.gov.sg>
2022-12-07 10:58:28 +0800

    [MOL-11420][JS] convert to axios

--- service-bundle-processor/scraper/api.ts
+++ service-bundle-processor/scraper/api.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { plainToInstance } from "class-transformer";
 import { TargetAudience, Topic } from "lib-content-taxanomy";
 import { Language } from "mol-lib-api-contract";
@@ -8,49 +9,39 @@ import {
 	GetServicesForBundleWithOptionsResponseBodyApiDomain,
 	getServicesForBundleWithOptionsServiceApi,
 } from "mol-lib-api-contract/content/mobile-content";
-import { RequestEndpoint } from "mol-lib-common";
 
 const LIFESG_BASE_URL = "https://www.dev.lifesg.io";
 
-const createClient = () => new RequestEndpoint().setBaseUrl(LIFESG_BASE_URL);
+const createClient = () =>
+	axios.create({
+		baseURL: LIFESG_BASE_URL,
+	});
 
 export const getServiceBundles = async () => {
-	const { body } = await createClient()
-		.setOptions({
-			json: true,
-		})
-		.get(getServiceBundlesServiceApi.path);
-
-	return plainToInstance(GetServiceBundleResponseBodyApiDomain, body).data;
+	const { data } = await createClient().get(getServiceBundlesServiceApi.path);
+	return plainToInstance(GetServiceBundleResponseBodyApiDomain, data).data;
 };
 
 export const getServicesForBundle = async (serviceBundleId: string) => {
-	const { body } = await createClient()
-		.setOptions({
-			json: true,
-		})
-		.get(getServicesForBundleWithOptionsServiceApi.path, {
-			params: {
-				serviceBundleId,
-				type: "all",
-			},
-		});
-	return plainToInstance(GetServicesForBundleWithOptionsResponseBodyApiDomain, body.data);
+	const { data } = await createClient().get(getServicesForBundleWithOptionsServiceApi.path, {
+		params: {
+			serviceBundleId,
+			type: "all",
+		},
+	});
+	return plainToInstance(GetServicesForBundleWithOptionsResponseBodyApiDomain, data.data);
 };
 
 export const getArticlesAndSchemes = async (topics: Topic[], targetAudiences: TargetAudience[]) => {
-	const { body } = await createClient()
-		.setOptions({
-			json: true,
-		})
-		.post("/content/api/v1/queryBundledArticlesAndSchemes", {
-			body: GetBundledArticlesAndSchemes.Transformers.transformGetBundledArticlesAndSchemesRequestApiDtoToDomain({
-				language: Language.MOLLanguage.ENGLISH,
-				topics,
-				targetAudiences,
-				limit: 5,
-			}),
-		});
+	const { data } = await createClient().post(
+		"/content/api/v1/queryBundledArticlesAndSchemes",
+		GetBundledArticlesAndSchemes.Transformers.transformGetBundledArticlesAndSchemesRequestApiDomainToDto({
+			language: Language.MOLLanguage.ENGLISH,
+			topics,
+			targetAudiences,
+			limit: 5,
+		}),
+	);
 
-	return GetBundledArticlesAndSchemes.Transformers.transformGetBundledArticlesAndSchemesResponseApiDtoToDomain(body);
+	return GetBundledArticlesAndSchemes.Transformers.transformGetBundledArticlesAndSchemesResponseApiDtoToDomain(data);
 };
